0009ff0ffc47c68ab81c36bed2a60a6e42499c66
mif <mif.open@gmail.com>
2017-12-08 13:36:21 +0500

    now row always has cells

--- ReportDotNet.Docx/Converters/RowConverter.cs
+++ ReportDotNet.Docx/Converters/RowConverter.cs
@@ -8,49 +8,44 @@ using Table = ReportDotNet.Core.Table;
 
 namespace ReportDotNet.Docx.Converters
 {
-    internal static class RowConverter
-    {
-        public static TableRow Convert(Row row,
-                                       WordprocessingDocument document,
-                                       Table table,
-                                       int[] tableWidths)
-        {
-            var docxRow = new TableRow();
-            var parameters = row.Parameters;
-            if (parameters.Height.HasValue)
-            {
-                docxRow.TableRowProperties = new TableRowProperties();
-                docxRow.TableRowProperties.AppendChild(new TableRowHeight
-                                                       {
-                                                           Val = (uint) parameters.Height.Value * OpenXmlUnits.Dxa,
-                                                           HeightType = ConvertHeightType(parameters.HeightType)
-                                                       });
-            }
+	internal static class RowConverter
+	{
+		public static TableRow Convert(Row row,
+									   WordprocessingDocument document,
+									   Table table,
+									   int[] tableWidths)
+		{
+			var docxRow = new TableRow();
+			var parameters = row.Parameters;
+			if (parameters.Height.HasValue)
+			{
+				docxRow.TableRowProperties = new TableRowProperties();
+				docxRow.TableRowProperties.AppendChild(new TableRowHeight
+													   {
+														   Val = (uint) parameters.Height.Value * OpenXmlUnits.Dxa,
+														   HeightType = ConvertHeightType(parameters.HeightType)
+													   });
+			}
 
-            var spans = Helpers.GetSpans(tableWidths, row.GetWidths());
-            if (parameters.GetCells().Any())
-            {
-                var cells = parameters.GetCells().ToArray();
-                for (var i = 0; i < cells.Length; i++)
-                    docxRow.AppendChild(CellConverter.Convert(cells[i], document, table, spans[i]));
-            }
-            else
-                docxRow.AppendChild(CellConverter.Convert(Factories.Cell().Build(), document, table, tableWidths.Sum()));
+			var spans = Helpers.GetSpans(tableWidths, row.GetWidths());
+			var cells = parameters.GetCells().ToArray();
+			for (var i = 0; i < cells.Length; i++)
+				docxRow.AppendChild(CellConverter.Convert(cells[i], document, table, spans[i]));
 
-            return docxRow;
-        }
+			return docxRow;
+		}
 
-        private static EnumValue<HeightRuleValues> ConvertHeightType(RowHeightType heightType)
-        {
-            switch (heightType)
-            {
-                case RowHeightType.Exact:
-                    return HeightRuleValues.Exact;
-                case RowHeightType.AtLeast:
-                    return HeightRuleValues.AtLeast;
-                default:
-                    throw new ArgumentOutOfRangeException(nameof(heightType), heightType, null);
-            }
-        }
-    }
+		private static EnumValue<HeightRuleValues> ConvertHeightType(RowHeightType heightType)
+		{
+			switch (heightType)
+			{
+				case RowHeightType.Exact:
+					return HeightRuleValues.Exact;
+				case RowHeightType.AtLeast:
+					return HeightRuleValues.AtLeast;
+				default:
+					throw new ArgumentOutOfRangeException(nameof(heightType), heightType, null);
+			}
+		}
+	}
 }
